19864d92828d326e7e3c0a32395ba713b6a5b15e
Lucas Begnaud <lucasbegnaud@gmail.com>
2024-11-21 21:23:41 -0700

    Added another codewar challenge

---
+++ 11-21-24/main.js
@@ -0,0 +1,9 @@
+//8kyu - Opposites Attract//
+
+function lovefunc(flower1, flower2){
+    if(flower1 % 2 == 0 && flower2 % 2 !== 0 || flower1 % 2 !== 0 && flower2 % 2 == 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
