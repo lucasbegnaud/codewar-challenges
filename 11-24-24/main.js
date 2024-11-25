9bf2c3883423d8398d01cbbffb61c35f2f792e33
Lucas Begnaud <lucasbegnaud@gmail.com>
2024-11-24 19:05:29 -0700

    Entering another codewar

---
+++ 11-24-24/main.js
@@ -0,0 +1,9 @@
+//8kyu - Sum Arrays//
+
+function sum (numbers) {
+    let sumOfNum = 0;
+    for(let i = 0; i < numbers.length; i++){
+      sumOfNum += numbers[i];
+    }
+    return sumOfNum;
+  }
