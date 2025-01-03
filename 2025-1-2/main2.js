c2b829a743481d72c0667d92191cb353b1fe411f
Lucas Begnaud <lucasbegnaud@gmail.com>
2025-01-02 23:28:10 -0700

    Added two daily codewars

---
+++ 2025-1-2/main2.js
@@ -0,0 +1,9 @@
+//8kyu - Grasshopper - Personalized Message//
+
+function greet (name, owner) {
+    if(name === owner){
+      return 'Hello boss'
+    } else {
+      return 'Hello guest'
+    }
+}
