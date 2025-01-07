406b81758412cea7d4b3727655659d188eeb027a
Lucas Begnaud <lucasbegnaud@gmail.com>
2025-01-06 22:26:34 -0700

    Added my two daily codewars.

---
+++ 2025-1-6/main1.js
@@ -0,0 +1,5 @@
+//8kyu - Third Angle of a Triangle//
+
+function otherAngle(a, b) {
+    return 180 - a - b
+}
---
+++ 2025-1-6/main2.js
@@ -0,0 +1,9 @@
+//8kyu - L1: Set Alarm//
+
+function setAlarm(employed, vacation){
+    if(employed === true && vacation !== true){
+      return true
+    } else {
+      return false;
+    }
+}
