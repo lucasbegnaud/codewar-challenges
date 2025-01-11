52fafd93af86bea263a2078e49ceaba8ebd819a9
Lucas Begnaud <lucasbegnaud@gmail.com>
2025-01-10 22:51:33 -0700

    Added two daily codewars

---
+++ 2025-1-10/checkForFactor.js
@@ -0,0 +1,9 @@
+//8kyu - Grasshopper - Check for factor//
+
+function checkForFactor (base, factor) {
+  if (base % factor === 0){
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ 2025-1-10/keepUp.js
@@ -0,0 +1,9 @@
+//8kyu - Keep up the hoop//
+
+function hoopCount (n) {
+    if(n >= 10){
+      return "Great, now move on to tricks";
+    } else {
+      return "Keep at it until you get it";
+    }
+}
