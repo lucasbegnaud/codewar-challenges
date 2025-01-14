0d5e7c3afc91790ab94caf5cf60f808fa0a89157
Lucas Begnaud <lucasbegnaud@gmail.com>
2025-01-13 18:31:01 -0700

    Added two daily codewars, Twice as old and Removing Elements

---
+++ 2025-01-13/removeElement.js
@@ -0,0 +1,11 @@
+//8kyu - Removing Elements//
+
+function removeEveryOther(arr){
+    let newArr = []
+    for(let i = 0; i < arr.length; i++){
+      if(i % 2 === 0){
+        newArr.push(arr[i])
+      }
+    }
+    return newArr;
+}
---
+++ 2025-01-13/twiceAsOld.js
@@ -0,0 +1,5 @@
+//8kyu - Twice as old//
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    return Math.abs(dadYearsOld - (sonYearsOld * 2));
+}
